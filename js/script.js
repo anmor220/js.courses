966d0e70119911895619b0d99a3a34959186b986
Rudaya Anastasia <anmor220@outlook.com>
2021-06-07 22:47:29 +0300

    Второй шаг

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 let numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
 
+
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,12 +11,36 @@ let personalMovieDB = {
     privat: false,
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+let a;
+let b;
+let c = 0;
+
+while(c > 2) {
+    a = prompt('Один из последних просмотренных фильмов?', '');
+    b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        c++;
+    }
+}
+//debugger;
+
 
 console.log(personalMovieDB);
+
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов', '');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель', '');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман', '');
+} else {
+    alert('Произошла ошибка', '');
+}
+
+(personalMovieDB.count < 10) ? alert('Просмотрено довольно мало фильмов', '') : 
+(personalMovieDB.count > 10 && personalMovieDB.count < 30) ? alert('Вы классический зритель', '') :
+(personalMovieDB.count > 30) ? alert('Вы киноман', '') : alert('Произошла ошибка', '');
+
