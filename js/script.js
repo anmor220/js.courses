3cc224c451f3a231ef8a14b1944991a650d57863
Rudaya Anastasia <anmor220@outlook.com>
2021-06-11 23:46:13 +0300

    Третий шаг

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
+    }
+}
+
+start();
 
 
 let personalMovieDB = {
@@ -12,35 +22,59 @@ let personalMovieDB = {
 };
 
 
-let a;
-let b;
-let c = 0;
-
-while(c > 2) {
-    a = prompt('Один из последних просмотренных фильмов?', '');
-    b = prompt('На сколько оцените его?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        c++;
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        let a = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres.push(a);
     }
 }
-//debugger;
+writeYourGenres();
+
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+
+function rememberMyFilms() {
+    let a;
+    let b;
+    let c = 0;
+    
+    while(c > 2) {
+        a = prompt('Один из последних просмотренных фильмов?', '');
+        b = prompt('На сколько оцените его?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            c++;
+        }
+    }
+    //debugger;
+}
+
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов', '');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель', '');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман', '');
+    } else {
+        alert('Произошла ошибка', '');
+    }
+    
+    // (personalMovieDB.count < 10) ? alert('Просмотрено довольно мало фильмов', '') : 
+    // (personalMovieDB.count > 10 && personalMovieDB.count < 30) ? alert('Вы классический зритель', '') :
+    // (personalMovieDB.count > 30) ? alert('Вы киноман', '') : alert('Произошла ошибка', '');
+    
+}
+
+detectPersonalLevel();
 
 
 console.log(personalMovieDB);
-
-
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов', '');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert('Вы классический зритель', '');
-} else if (personalMovieDB.count > 30) {
-    alert('Вы киноман', '');
-} else {
-    alert('Произошла ошибка', '');
-}
-
-(personalMovieDB.count < 10) ? alert('Просмотрено довольно мало фильмов', '') : 
-(personalMovieDB.count > 10 && personalMovieDB.count < 30) ? alert('Вы классический зритель', '') :
-(personalMovieDB.count > 30) ? alert('Вы киноман', '') : alert('Произошла ошибка', '');
-
